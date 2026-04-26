83eb1b376526d3a01cea7778930e5fcceeada7d9
MariaLuize <m.luize.s.pinheiro@gmail.com>
2026-04-26 11:03:20 -0400

    add model file

--- train_kinetic.py
+++ train_kinetic.py
@@ -1,9 +1,13 @@
 import kinetic
-from model import get_model
+# from model import get_model
+from model import UNetModel
 from data_loader import get_dataset
 
 def train():
     TRAIN_PATH = "gs://your-bucket/lulc_samples/train"
+    model_instance = UNetModel(input_shape=[None, None, len(optical_bands + optical_indices)], dropout_rate=dropout, loss=loss, metrics_list=metrics)
+    model          = model_instance.get_model()
+
     
     job = kinetic.create_job(
         model_fn=get_model,
